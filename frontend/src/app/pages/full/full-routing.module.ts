846b4200f575a2653fed9d3278867c7db8624968
Edwar Erazo <edwarerazo321@gmail.com>
2021-09-18 16:04:27 -0500

    front

--- frontend/src/app/pages/full/full-routing.module.ts
+++ frontend/src/app/pages/full/full-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdministratorComponent } from './administrator/administrator.component';
 import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
 
 const routes: Routes = [
+  { path: 'administrator', component: AdministratorComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'movies', component: MoviesComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
--- frontend/src/app/pages/full/full.module.ts
+++ frontend/src/app/pages/full/full.module.ts
@@ -4,10 +4,14 @@ import { CommonModule } from '@angular/common';
 import { FullRoutingModule } from './full-routing.module';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AdministratorComponent } from './administrator/administrator.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    AdministratorComponent,
+    MoviesComponent
   ],
   imports: [
     CommonModule,
